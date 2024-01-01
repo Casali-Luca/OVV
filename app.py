6f1a46189b82fb1a1245479f2a5cc4ea726448e1
Luca <casalixluca@gmail.com>
2024-01-01 12:10:22 +0100

    fixes #1 +  gitignore

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-from flask import Flask, request, jsonify, render_template, make_response
+from flask import Flask, request, jsonify, render_template
 from random import *
-import subprocess as sp
-from flask_mysqldb import MySQL
+from pymysql import *
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "centralina"
-
-mysql = MySQL(app)
-
 
 @app.route("/")
 def home():
-    # out = sp.run(["php", "index.php"], stdout=sp.PIPE)
-    # return out.stdout
-    return render_template("from.html")
-
+    return render_template("index.html")
 
 @app.route("/get-data/<temp>,<umidity>,<pm>")
 def get_data(temp, umidity, pm):
@@ -33,6 +22,19 @@ def get_data(temp, umidity, pm):
     return jsonify(data), 200
 
 
+@app.route("/print_all/<station>")
+def printall(station : str):
+    db = connect(host='localhost',
+                 user="root",
+                 password="",
+                 db="centralina")
+    cur = db.cursor()
+    cur.execute('SELECT * FROM ' + station)
+    results = cur.fetchall()
+    db.close()
+    return jsonify(results), 202
+
+
 @app.route("/insert-data/<temp>,<umidity>,<pm>")
 def insert_data(temp, umidity, pm):
     ins_data = {
@@ -45,14 +47,7 @@ def insert_data(temp, umidity, pm):
     if extra:
         ins_data["extra"] = extra
 
-    cursor = mysql.connection.cursor()
-    #ex:
-    #INSERT INTO `centrale` (`id`, `temperatura`, `umidita`, `pm`) VALUES (NULL, '28.7', '93.1', '1.1')
-    cursor.execute("INSERT INTO " + app.config["MYSQL_DB"] + " (`id`, `temperatura`, `umidita`, `pm`) VALUES (NULL,%f,%f,%f)", temp,umidity, pm)
-    mysql.connection.commit()
-    cursor.close()
-
-    return "dati inseriti:\n" +jsonify(ins_data), 200
+    return jsonify(ins_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
