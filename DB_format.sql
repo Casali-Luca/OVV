1a9cb4bea20f230e37b6fe72c01e303c88555112
TgCas24 <casalixluca@gmail.com>
2024-03-13 17:50:53 +0100

    migliorie API + rinnovamento DB
    distinzione GET / POST,  si ottiene l'ultima aggiunta nelle singole ricerche, gestione avvisi secondo 81/08

--- DB_format.sql
+++ DB_format.sql
@@ -1,10 +1,18 @@
 CREATE DATABASE centralina;
 USE centralina;
-CREATE TABLE centrale(
+CREATE TABLE DatiCentraline(
     id INT NOT NULL AUTO_INCREMENT,
+    codiceCentralina VARCHAR(7) NOT NULL,
     temperatura FLOAT NOT NULL,
     luce FLOAT NOT NULL,
     umidita FLOAT NOT NULL,
-    pm FLOAT NOT NULL,
+    pm FLOAT,
+
     PRIMARY KEY(id)
+);
+
+CREATE TABLE avvisi(
+    oraEvento DATETIME NOT NULL PRIMARY KEY,
+    idCentralina INT NOT NULL UNIQUE,
+    FOREIGN KEY (idCentralina) REFERENCES DatiCentraline(id)
 )
--- app.py
+++ app.py
@@ -3,57 +3,76 @@ from random import *
 from pymysql import *
 import python_weather
 import asyncio
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+CodiciStazioni = {
+    "centrale" : "CNTRmid",
+    "destra" : "CNTRdes",
+    "sinistra" : "CNTRsin"
+}
+
 #semplice funzionamento delle api del meteo (WIP)
-async def get_weather():
-    nomecity = "Bergamo"
-    #dopo aver scelto la città, andiamo a richiamare le api del meteo tramite la classe presente in pythonWeather "client"
-    async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-        weather = await client.get(f"{nomecity}")
-        #ritornerà una hash formata da ogni 3 ore con la relativa condizione metereologica
-        for forecast in weather.forecasts:
-            for hourly in forecast.hourly:
-                print(f' --> {hourly!r}')
-            break
+# async def get_weather():
+#     nomecity = "Bergamo"
+#     #dopo aver scelto la città, andiamo a richiamare le api del meteo tramite la classe presente in pythonWeather "client"
+#     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+#         weather = await client.get(f"{nomecity}")
+#         #ritornerà una hash formata da ogni 3 ore con la relativa condizione metereologica
+#         for forecast in weather.forecasts:
+#             for hourly in forecast.hourly:
+#                 print(f' --> {hourly!r}')
+#             break
 
 #route di partenza
 @app.route("/")
 def home():
-    #renderizza l'index.html (WIP)
+    #renderizza l'index.html (da ./templates)
     return render_template("index.html",)
 
 #va a stampare tutti i valori presenti in una tabella a scelta del db
-@app.route("/print_all/<station>", methods=['POST'])
+@app.route("/print_all/<station>", methods=['GET'])
 def printall(station : str):
-    if request.method=='POST':
+    if request.method=='GET':
         if request.args.get("ApplicationKey") == "oqwg-dash-jkbc-phuw-qgey-bhas-dapp":
             db = connect(host='localhost',
                             user="root",
                             password="",
                             db="centralina")
             cur = db.cursor()
-            cur.execute('SELECT * FROM ' + station)
+            codiceStation = CodiciStazioni[station.lower()]
+            if station =="":
+                cur.execute('SELECT * FROM DatiCentraline WHERE 1 ')
+            else:
+                cur.execute('SELECT * FROM DatiCentraline WHERE codiceCentralina = "' + codiceStation +'"')
+            
+            
             results = cur.fetchall()
             db.close()
             return jsonify(results), 202
         else:
-            return "something went wrong"
+            return "Chiave incorretta"
 
 #inserimento data da parte dell'arduino o qualsiasi richiesta di pull
 @app.route("/insert_data/<station>/<temp>,<umidity>,<pm>,<luce>", methods=['POST'])
 def insert_data(temp :str, umidity:str, pm:str, luce:str, station:str):
     if request.method=='POST':
         if request.args.get("ApplicationKey") == "oqwg-dash-jkbc-phuw-qgey-bhas-dapp":
-            if(station !=  "centrale"):
-                return render_template("error.html")
-            
+
+
+            #decrypting
             temp = decrypt(temp)
             umidity = decrypt(umidity)
             pm = decrypt(pm)
             luce = decrypt(luce)
 
+            if not (station.lower() in CodiciStazioni.keys()):
+                return "stazione non esistente"
 
+            #codice della centralina
+            codiceStation = CodiciStazioni[station.lower()]
 
             ins_data = {
                 "temperatura" : temp,
@@ -61,8 +80,7 @@ def insert_data(temp :str, umidity:str, pm:str, luce:str, station:str):
                 "pm" : pm,
                 "luce" : luce
             }
-            extra = request.args.get("extra")
-
+            
             db = connect(host='localhost',
                         user="root",
                         password="",
@@ -72,8 +90,14 @@ def insert_data(temp :str, umidity:str, pm:str, luce:str, station:str):
 
             #ex:
             #INSERT INTO `centrale` (`id`, `temperatura`, `umidita`, `pm`) VALUES (NULL, '28.7', '93.1', '1.1')
-            cur.execute("INSERT INTO `"+station+"` (id, temperatura, luce, umidita, pm) VALUES (NULL,"+ temp +"," + luce +","+ umidity+ "," + pm+")")
+            cur.execute('INSERT INTO DatiCentraline (id, codiceCentralina, temperatura, luce, umidita, pm) VALUES (NULL, "'+codiceStation +'",'+ temp +"," + luce +","+ umidity+ "," + pm+")")
             db.commit()
+            if int(temp)>45 or int(luce)>1000 or int(luce)<500 or int(temp)<18 or int(temp)>25 or int(umidity)<40 or int(umidity) >50:
+                cur.execute("INSERT INTO avvisi (oraEvento, idCentralina) VALUES (NOW(), (SELECT MAX(id) FROM DatiCentraline))")
+                db.commit()
+
+
+
             cur.close()
             #ritorno del json di ciò che abbiamo appena inserito
             return jsonify(ins_data), 200
@@ -82,7 +106,7 @@ def insert_data(temp :str, umidity:str, pm:str, luce:str, station:str):
 
 
 #directory adibita alla chiamata API
-@app.route('/single_api/<api_name>', methods=['POST'])
+@app.route('/single_api/<api_name>', methods=['GET'])
 def get_api_value(api_name):
     try:
         value = get_api_data(api_name)
@@ -92,7 +116,7 @@ def get_api_value(api_name):
 
 
 def get_api_data(api_name):
-    if request.method=='POST':
+    if request.method=='GET':
         if request.args.get("ApplicationKey") == "oqwg-dash-jkbc-phuw-qgey-bhas-dapp":
             db = connect(host='localhost',
                             user="root",
@@ -112,7 +136,7 @@ def get_api_data(api_name):
             elif api_name == 'light':
                 var = "luce"
 
-            sql = "SELECT "+ var +" FROM centrale"
+            sql = "SELECT " + var + " FROM DatiCentraline ORDER BY id DESC LIMIT 1"
             cur.execute(sql)
             
             results = cur.fetchall()
@@ -145,5 +169,5 @@ def decrypt(stringa : str):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5500, threaded=True)
 
